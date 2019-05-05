80aef3c3a37836195e8a435ada07ab720988b885
Morten Sickel <morten@sickel.net>
2019-05-05 23:00:13 +0200

    DHT-thread

--- controller.py
+++ controller.py
@@ -1,16 +1,25 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import time,threading
 
+import Adafruit_DHT
+
+
+DHTSENSOR = Adafruit_DHT.DHT22
+
 lastpush=0
+DHTPIN = 4
 BTPIN=22
 LEDPIN=18
 t=None
 ontime=15.0
 
+DHTWAIT=10
+
 def button_callback(channel):
     global LEDPIN
     global lastpush
     global t
+    
     print("Button was pushed!")
     now=time.time()
     print(now)
@@ -27,14 +36,27 @@ def button_callback(channel):
     else:
         turnoff()
         t.cancel() # Do now want to have a timer hanging around to turn off the led later on.
-
+    
+    
+    
 def turnoff():
     global LEDPIN
     print("LED off")
     print(time.time())
     GPIO.output(LEDPIN,GPIO.LOW)
 
-
+def readDHT(evt):
+    global DHTSENSOR
+    global DHTPIN
+    global DHTWAIT
+    while True:
+        if evt.isSet():
+            return()
+        humidity, temperature = Adafruit_DHT.read_retry(DHTSENSOR, DHTPIN)
+        print(humidity)
+        print(temperature)
+        time.sleep(DHTWAIT)
+    
 
 
 GPIO.setwarnings(False) # Ignore warning for now
@@ -43,7 +65,12 @@ GPIO.setup(BTPIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an inp
 GPIO.setup(LEDPIN,GPIO.OUT) # Want to make sure we start in a known state
 GPIO.output(LEDPIN,GPIO.LOW)
 GPIO.add_event_detect(BTPIN,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+stopDHT=threading.Event()
+DHTthread=threading.Thread(target = readDHT, args = (stopDHT, ))
+DHTthread.start()
 message = input("Press enter to quit\n\n") # Run until someone presses enter
+stopDHT.set()
+DHTthread.join()
 GPIO.cleanup() # Clean up
 
 
