133d25a0ac970cf30d481dabc295a0fb0918f7f5
Morten Sickel <morten@sickel.net>
2019-05-11 12:48:48 +0200

    Writing temp and humidity to LCD

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GardenController
 Using to take various input and control a watering system in my greenhouse
+
+For lcd, using library mikezlcd.py from https://www.mikronauts.com/raspberry-pi/raspberry-pi-1602-and-2004-lcd-interfacing/ download: https://www.mikronauts.com/wp-content/upload/2014/11/mikezlcd.py_.txt 
--- controller.py
+++ controller.py
@@ -3,17 +3,24 @@ import time,threading
 
 import Adafruit_DHT
 
+from mikezlcd import *
+lcd = lcd_module(2004, 25, 24, 22, 18, 17,23)
 
 DHTSENSOR = Adafruit_DHT.DHT22
 
 lastpush=0
 DHTPIN = 4
-BTPIN=22
-LEDPIN=18
+BTPIN=7
+LEDPIN=8
 t=None
-ontime=15.0
+ontime=120.0
 
-DHTWAIT=10
+TIMEFORMAT="%m/%d/%Y, %H:%M:%S.%f"
+
+DHTWAIT=30
+
+def fmttime(timefloat):
+    return time.strftime(TIMEFORMAT,time.localtime(timefloat))
 
 def button_callback(channel):
     global LEDPIN
@@ -22,7 +29,7 @@ def button_callback(channel):
     
     print("Button was pushed!")
     now=time.time()
-    print(now)
+    print(fmttime(now))
     if now - lastpush < 1: # Debouching
         print("whopsa, wait a moment")
         return
@@ -35,32 +42,58 @@ def button_callback(channel):
         print("ON")
     else:
         turnoff()
-        t.cancel() # Do now want to have a timer hanging around to turn off the led later on.
-    
+        
+
+
+def handleht(hum,temp,mintemp,maxtemp):
+    global lcd
+    print("Humidity",hum,"%")
+    print("Temperature",temp,"°C")
+    lcd.disp(0,0,"Temp {:5.1f} oC".format(temp))
+    lcd.disp(0,1,"     {:5.1f} - {:5.1f}".format(mintemp,maxtemp))
+    lcd.disp(0,2,"Fukt {:5.1f} %".format(hum))
     
     
 def turnoff():
     global LEDPIN
     print("LED off")
-    print(time.time())
+    print(fmttime(time.time()))
     GPIO.output(LEDPIN,GPIO.LOW)
-
+    if not t == None:
+        t.cancel() # Do now want to have a timer hanging around to turn off the led later on.
+    
 def readDHT(evt):
     global DHTSENSOR
     global DHTPIN
     global DHTWAIT
+    mintemp=100
+    maxtemp=-100
+    dhtread=0
     while True:
         if evt.isSet():
             return()
-        humidity, temperature = Adafruit_DHT.read_retry(DHTSENSOR, DHTPIN)
-        print(humidity)
-        print(temperature)
-        time.sleep(DHTWAIT)
+        now=time.time()
+        if now-dhtread>DHTWAIT: # Cannot sleep for DHTWAIT as it will stop processing of ev
+            print("DHTread:")
+            dhtread=now
+            print()
+            humidity, temperature = Adafruit_DHT.read_retry(DHTSENSOR, DHTPIN)
+            mintemp=min(mintemp,temperature)
+            maxtemp=max(maxtemp,temperature)
+            handleht(humidity,temperature,mintemp,maxtemp)
+            if evt.isSet():
+                return()
+            if humidity>75:
+                state=GPIO.input(LEDPIN)
+                if not state==0:
+                    print("Too humid, turning off")
+                    turnoff()
+        time.sleep(1)
     
 
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+#GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(BTPIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(LEDPIN,GPIO.OUT) # Want to make sure we start in a known state
 GPIO.output(LEDPIN,GPIO.LOW)
@@ -71,6 +104,7 @@ DHTthread.start()
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 stopDHT.set()
 DHTthread.join()
+turnoff()
 GPIO.cleanup() # Clean up
 
 
